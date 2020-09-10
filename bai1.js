f1f7664c548250a9c9ed7ea2564494aa399548bc
phamdao0220 <phamd1411200220@gmail.com>
2020-09-10 15:14:08 +0700

    khai bao bang

---
+++ bai1.js
@@ -0,0 +1,12 @@
+let f =20.5
+let b =true
+let s = 'ha noi'
+let i = 10
+
+document.write('1 = ' + i);
+document.write('<br/>');
+document.write('f = ' + f);
+document.write('<br/>');
+document.write('b = ' + b);
+document.write('<br/>');
+document.write('s = ' + s);
---
+++ bai3.js
@@ -0,0 +1,10 @@
+a = prompt('nhap bien a');
+b = prompt( 'nhap bien b');
+if (a%b==0){
+    alert(" a chia het cho b");
+
+} else {
+    alert("a khong chia het cho b");
+
+}
+
